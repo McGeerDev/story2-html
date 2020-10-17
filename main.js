c0b0c734daf0bd8a1a45b1a0fd3347bbf82c88c7
Mitchell Horne <mitchellh@ninety10.co.za>
2020-10-17 18:58:50 +0200

    Add all mobile table instances

--- main.js
+++ main.js
@@ -21,3 +21,41 @@ const toggleMenu = (event) => {
 }
 
 document.getElementById("nav-menu-button").addEventListener("click", toggleMenu);
+
+// Select packages in mobile
+const previousPackage = () => {
+    const packages = Array.prototype.slice.call(document.getElementsByClassName("package"));
+    const [activePackage] = document.getElementsByClassName("package active");
+
+    packages.forEach((pkg, index) => {
+        if (pkg === activePackage) {
+            if (index === 0) {
+                activePackage.classList.remove("active");
+                return packages[packages.length - 1].classList.add("active");
+            }
+
+            activePackage.classList.remove("active");
+            return packages[index - 1].classList.add("active");
+        }
+    });
+}
+
+const nextPackage = () => {
+    const packages = Array.prototype.slice.call(document.getElementsByClassName("package"));
+    const [activePackage] = document.getElementsByClassName("package active");
+
+    packages.forEach((pkg, index) => {
+        if (pkg === activePackage) {
+            if (index === (packages.length -1)) {
+                activePackage.classList.remove("active");
+                return packages[0].classList.add("active");
+            }
+
+            activePackage.classList.remove("active");
+            return packages[index + 1].classList.add("active");
+        }
+    });
+}
+
+document.getElementById("prev-package").addEventListener("click", previousPackage);
+document.getElementById("next-package").addEventListener("click", nextPackage);
